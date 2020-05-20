2ff07b21c7730c725578f8805b9f53f87c65c9a4
robings <58939401+robings@users.noreply.github.com>
2020-05-20 22:06:44 +0100

    changed colours for worksheet

--- viewFunctions.js
+++ viewFunctions.js
@@ -2,10 +2,10 @@ function generateWorksheetView(questions) {
     let worksheetHTML = ''
 
     questions.forEach((question, key) => {
-        let questionHTML = '<div class="question">'
+        let questionHTML = '<div class="questionContainer"><div class="question">'
         questionHTML += `<div>Q${key+1}.</div><div>${question.x}</div><div>&nbsp;${question.operand}&nbsp;</div>`
         questionHTML += `<div>${question.y}</div><div>&nbsp;=&nbsp;</div>`
-        questionHTML += '<div class="answerBox"></div><div class="tickBox"></div></div>'
+        questionHTML += '<div class="answerBox"></div><div class="tickBox"></div></div></div>'
         worksheetHTML += questionHTML
     })
 
